f1317dd48f05baddc2cea9437b257574be433ac6
Alexandre <alexandreb09@yahoo.fr>
2019-02-21 22:11:12 +0100

    Affichage + calcul

--- Code/app/src/main/java/calc/isima/fr/tp1_modele_calculatrice/CalcActivity.java
+++ Code/app/src/main/java/calc/isima/fr/tp1_modele_calculatrice/CalcActivity.java
@@ -4,16 +4,27 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
+import com.udojava.evalex.Expression;
+
+import org.apache.commons.lang3.ArrayUtils;
 import org.apache.commons.lang3.StringUtils;
+
+import java.util.Arrays;
+import java.util.List;
+
+
 
 /**
  * Activity qui affiche la calculatrice.
  */
-public class CalcActivity extends Activity {
 
+public class CalcActivity extends Activity implements View.OnClickListener {
     private static final String TAG = CalcActivity.class.getName();
+
 
     /** Nom du fichier dans lequel seront stockés les expressions et les résultats correspondants. */
     public static final String SAVE_FILE_NAME = "history";
@@ -40,12 +51,26 @@ public class CalcActivity extends Activity {
     /** View qui permet d'afficher l'expression mathématique à l'utilisateur. */
     private TextView expressionView;
 
+    Button myButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.calc_activity);
 
+        resultView = findViewById(R.id.resultat);
+        expressionView = findViewById(R.id.operation);
+
+        for (int number : numbers) {
+            myButton = findViewById(number);
+            myButton.setOnClickListener(this);
+        }
+
+        for (int number : operators) {
+            myButton = findViewById(number);
+            myButton.setOnClickListener(this);
+        }
 
     }
 
@@ -56,7 +81,7 @@ public class CalcActivity extends Activity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         super.onCreateOptionsMenu(menu);
-        getMenuInflater().inflate(R.menu.calc, menu);
+        // getMenuInflater().inflate(R.menu.calc, menu);
 
         return true;
     }
@@ -75,6 +100,19 @@ public class CalcActivity extends Activity {
         }
     }
 
+    @Override
+    public void onClick(View v){
+        int view_id = v.getId();
+
+        if (ArrayUtils.contains(operators,view_id)){
+            onOperatorClicked(view_id);
+        }
+        else {
+            onNumberClicked(view_id);
+        }
+    }
+
+
     /**
      * Méthode à appeler pour sauvegarder l'expression et son résultat dans l'historique, lorsque l'utilisateur clique
      * sur le bouton Save.
@@ -88,7 +126,9 @@ public class CalcActivity extends Activity {
      * @param numberId Identifiant de la View cliquée.
      */
     private void onNumberClicked(int numberId) {
-        // TODO
+        Button b = findViewById(numberId);
+        String charToAdd = b.getText().toString();
+        updateExpression(charToAdd, false);
     }
 
     /**
@@ -96,7 +136,19 @@ public class CalcActivity extends Activity {
      * @param operatorId Identifiant de la View cliquée.
      */
     private void onOperatorClicked(int operatorId) {
-        // TODO
+        Button b = findViewById(operatorId);
+        String charToAdd = b.getText().toString();
+        if (operators[4] == operatorId){
+            expression = expression.substring(0, expression.length() - 1);
+            resultView.setText(expression);
+            expressionView.setText(expression);
+        }
+        else if (operators[5] == operatorId){
+            updateExpression(".", true);
+        }
+        else{
+            updateExpression(charToAdd, true);
+        }
     }
 
     /**
@@ -108,8 +160,8 @@ public class CalcActivity extends Activity {
         if (isOperator && expressionEndsWithOperator()) {
             expression = StringUtils.substring(expression, 0, -1);
         }
-
-        // TODO : Mettre à jour l'expression et la vue
+        expression += val;                                                                          // Ajout nouveau caractère
+        expressionView.setText(expression);
 
         if (!isOperator) {
             updateResult();
@@ -121,9 +173,10 @@ public class CalcActivity extends Activity {
      * @return True si l'expression se termine par un opérateur, false sinon.
      */
     private boolean expressionEndsWithOperator() {
-        // TODO
-
-        return false;
+        int lenght = expression.length();                                                           // Longueur expression
+        String last_letter = expression.substring(lenght - 1);                                      // Dernier caractère
+        List<String> operators = Arrays.asList("+", "-", "/", "*");                                 // Liste de opérateur
+        return operators.contains(last_letter);                                                     // dernier caract dans liste opérateurs ?
     }
 
     /**
@@ -133,8 +186,7 @@ public class CalcActivity extends Activity {
      */
     private String evaluateExpression() {
         String eval = "";
-
-        // TODO
+        eval += new Expression(expression).eval().toPlainString();
 
         return eval;
     }
@@ -143,6 +195,6 @@ public class CalcActivity extends Activity {
      * Cette méthode doit mettre à jour la View d'affichage du résultat.
      */
     private void updateResult() {
-        // TODO Mettre à jour la vue
+        resultView.setText(evaluateExpression());
     }
 }
